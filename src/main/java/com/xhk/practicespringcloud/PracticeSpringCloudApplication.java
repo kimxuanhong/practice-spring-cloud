4fa76e23766735826b88922b54c549082b9de60f
hong.kim <hong.kim@bstarsolutions.com>
2023-07-26 09:45:36 +0700

    init app

---
+++ src/main/java/com/xhk/practicespringcloud/PracticeSpringCloudApplication.java
@@ -0,0 +1,13 @@
+package com.xhk.practicespringcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeSpringCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeSpringCloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xhk/practicespringcloud/PracticeSpringCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xhk.practicespringcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeSpringCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
